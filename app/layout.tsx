a928294b38e22ebad929400ea86f1888a52ddf8d
Umar Nadeem <umarnadeem24@icloud.com>
2025-11-27 15:00:51 +0500

    Landing Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body>
         <Header />
         {children}
-       
+       <Footer />
       </body>
     </html>
   );
